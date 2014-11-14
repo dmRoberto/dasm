9281590e1d713075970a780982fb2daa96881ef7
bd0101 <roberto.d20@gmail.com>
2014-11-14 19:13:07 +0100

    Conexion api

--- ws.example/gen/com/example/ws/example/R.java
+++ ws.example/gen/com/example/ws/example/R.java
@@ -14,8 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f060000;
+        public static final int RelativeLayout1=0x7f060000;
         public static final int button=0x7f060001;
+        public static final int numDni=0x7f060002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- ws.example/src/com/example/ws/example/MainActivity.java
+++ ws.example/src/com/example/ws/example/MainActivity.java
@@ -1,29 +1,49 @@
 package com.example.ws.example;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private EditText dni;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		dni = (EditText) findViewById(R.id.numDni);
 	}
 
 	public void consulta(View v) {
-		new consultaBD().execute();
+		new consultaBD().execute(dni.getText().toString());
 	}
 
-	private class consultaBD extends AsyncTask<Void, Void, Void> {
+	private class consultaBD extends AsyncTask<String, Void, String> {
 		private ProgressDialog pDialog;
+
+		private Boolean pete;
+		private final String URL = "http://demo.calamar.eui.upm.es/dasmapi/v1/miw08/fichas";
+		
 
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
+			pete = false;
 			pDialog = new ProgressDialog(MainActivity.this);
 			pDialog.setMessage("Conectando con la BBDD...");
 			pDialog.setIndeterminate(false);
@@ -32,20 +52,68 @@ public class MainActivity extends Activity {
 		}
 
 		@Override
-		protected Void doInBackground(Void... params) {
-
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		protected String doInBackground(String... params) {
+			String dni = params[0];
+			String url_final = URL;
+			
+			if (!dni.equals("")){
+				url_final += "/"+ dni;
 			}
-			return null;
+			
+			AndroidHttpClient cliente = AndroidHttpClient
+					.newInstance("AndroidHttpClient");
+			HttpGet httpGet = new HttpGet(url_final);
+			String responseData = "";
+
+			
+			try {
+				HttpResponse reponse = cliente.execute(httpGet);
+				responseData = EntityUtils.toString(reponse.getEntity());
+				cliente.close();
+			} catch (IOException e) {
+				pete = true;
+				Log.e("Peto la operacion...", e.toString());
+			}
+			return responseData;
 		}
 
 		@Override
-		protected void onPostExecute(Void params) {
+		protected void onPostExecute(String params) {
 			pDialog.dismiss();
+			String message = "";
+			if (pete) {
+				message = "Se produjo un error al realizar la consulta";
+				Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG)
+						.show();
+				return;
+			}
+	
+			try {
+				JSONArray jsonData = new JSONArray(params);
+				int regNum = jsonData.getJSONObject(0).getInt("NUMREG");
+				switch (regNum) {
+				case -1:
+					message = "Se produjo un error al realizar la consulta";
+					break;
+				case 0:
+					message = "No se encontraron datos";
+					break;
+				case 1:
+					message = "Se encontró un registro en la BBDD";
+					break;
+				default:
+					message = "Se encontraron " + regNum
+							+ " regitros en la BBDD";
+					break;
+				}
+				Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG)
+						.show();
+			} catch (JSONException e) {
+				pete = true;
+				Log.e("Peto la operacion...", e.toString());
+				Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG)
+						.show();
+			}
 		}
 
 	}
