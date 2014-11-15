2f369bcc6310fe042c2c149938cfc94b0256cb37
bd0101 <roberto.d20@gmail.com>
2014-11-15 12:06:05 +0100

    modificaciones vista

--- practicadasm/gen/com/example/practicadasm/R.java
+++ practicadasm/gen/com/example/practicadasm/R.java
@@ -14,14 +14,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060000;
+        public static final int LinearLayout1=0x7f060000;
+        public static final int editTextDNIInit=0x7f060002;
+        public static final int imageButtonCreateInit=0x7f060003;
+        public static final int imageButtonEditInit=0x7f060005;
+        public static final int imageButtonRemoveInit=0x7f060006;
+        public static final int imageButtonSearchInit=0x7f060004;
+        public static final int textView1=0x7f060001;
     }
     public static final class layout {
         public static final int activity_dasm=0x7f030000;
+        public static final int activity_dasm_borrado=0x7f030001;
+        public static final int activity_dasm_consulta=0x7f030002;
+        public static final int activity_dasm_insercion=0x7f030003;
+        public static final int activity_dasm_modificacion=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int opServicioWeb=0x7f040002;
+        public static final int title_activity_dasm_borrado=0x7f040006;
+        public static final int title_activity_dasm_consulta=0x7f040003;
+        public static final int title_activity_dasm_insercion=0x7f040004;
+        public static final int title_activity_dasm_modificacion=0x7f040005;
     }
     public static final class style {
         /** 
